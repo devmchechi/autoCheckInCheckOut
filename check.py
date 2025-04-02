bf1f6c4cc01ea9e5c53b065a3aa41aa0810666bb
Dev Chechi <devmchechi@gmail.com>
2025-04-02 14:24:12 -0700

    Create check.py

---
+++ check.py
@@ -0,0 +1,70 @@
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+user_data_dir = r"C:\Users\{user_name}\AppData\Local\Google\Chrome\User Data" #Check Google User Data Path
+profile_name = "Default"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}") 
+chrome_options.add_argument(f"--profile-directory={profile_name}") 
+chrome_options.add_argument("--no-sandbox") 
+chrome_options.add_argument("--disable-dev-shm-usage")  
+chrome_options.add_argument("--disable-gpu")  
+chrome_options.add_argument("--remote-debugging-port=9222")  
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+link = "{workday_user_link}"
+def pressButton(path):
+    try:
+        button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(path))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(path))
+        driver.execute_script("arguments[0].click();", button)
+        print("Pressed Button")
+    except Exception as e:
+        print("Button not found", e)
+
+userTime = "{checkInOrOut_time}" # In 24 Hour %H:%M Format
+
+def waitTill(userTime):
+    currentTime = datetime.datetime.now().strftime("%H:%M")
+    while (currentTime != userTime): 
+        print(f"Current time: {currentTime}. Waiting for {userTime}...")
+        time.sleep(10)
+        currentTime = datetime.datetime.now().strftime("%H:%M")
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.get(link)
+    print(f"Target time {userTime} reached! Proceeding to press the button...")
+
+def inOrOut(checkInOrOut: bool):
+    waitTill(userTime)
+    
+    if checkInOrOut:
+        pressButton((By.XPATH, "//button[contains(@aria-label, 'Check In')]"))
+        print("Pressed Clock In")
+    else:
+        pressButton((By.XPATH, "//button[contains(@aria-label, 'Check Out')]"))
+        print("Pressed Clock Out")
+
+        pressButton((By.XPATH, "//label[text()='Out']"))
+        print("Pressed Out")
+    
+    pressButton((By.XPATH, "//button[@title='OK']"))
+    print("Pressed Okay")
+
+    pressButton((By.XPATH, "//button[@title='Done']"))
+    print("Pressed Done")
+
+
+try:
+    inOrOut(True) # if true = check in, else = check out
+except Exception as e:
+    print("Something went wrong", e)
